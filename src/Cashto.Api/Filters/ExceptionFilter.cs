30153737253cb2ba6cd74b3f071231c24a7decc1
EoBrad <joao.alencar099@gmail.com>
2024-11-15 14:16:49 -0300

    Feat: Create ExceptionFilter Class

---
+++ src/Cashto.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,39 @@
+using Cashto.Communication.Responses;
+using Cashto.Exception;
+using Cashto.Exception.ExceptionBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Cashto.Api.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is CashtoException)
+        {
+            HandleProjectException(context);
+        }
+        else
+        {
+            ThrowUnkowError(context);
+        }
+    }
+
+    private void HandleProjectException(ExceptionContext context)
+    {
+        var cashFlowException = (CashtoException)context.Exception;
+        var errorResponse = new ResponseErrorJson(cashFlowException.GetErrors());
+
+        context.HttpContext.Response.StatusCode = cashFlowException.StatusCode;
+        context.Result = new ObjectResult(errorResponse);
+    }
+
+    private void ThrowUnkowError(ExceptionContext context)
+    {
+        var errorResponse = new ResponseErrorJson(ResourceErrorMessage.UNKNOWN_ERROR);
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(errorResponse);
+    }
+}
