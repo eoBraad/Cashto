4a5550383ad6575f171693f42c8a32ed2f6d46c6
EoBrad <joao.alencar099@gmail.com>
2024-11-15 12:24:35 -0300

    Feat: Add user Entity

---
+++ src/Cashto.Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Cashto.Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string? Name { get; set; }
+    public string? LastName { get; set; }
+    public string? Password { get; set; }
+    public string? Email { get; set; }
+}
