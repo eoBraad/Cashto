e821472070c143ad30aec8f7602ccb98d013b17b
EoBrad <joao.alencar099@gmail.com>
2024-11-15 14:17:10 -0300

    Feat: Configure the exception filter in the project

--- src/Cashto.Api/Program.cs
+++ src/Cashto.Api/Program.cs
@@ -1,19 +1,18 @@
+using Cashto.Api.Filters;
 using Cashto.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
+builder.Services.AddMvc(opt => opt.Filters.Add<ExceptionFilter>());
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
