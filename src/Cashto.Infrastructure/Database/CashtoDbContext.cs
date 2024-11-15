32fc4a2da419c92f44c115a8a668ecb470352ded
EoBrad <joao.alencar099@gmail.com>
2024-11-15 12:26:05 -0300

    Feat: Add User in DbContext

--- src/Cashto.Infrastructure/Database/CashtoDbContext.cs
+++ src/Cashto.Infrastructure/Database/CashtoDbContext.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Cashto.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cashto.Infrastructure.Database;
 
 public class CashtoDbContext(DbContextOptions<CashtoDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<User> Users { get; set; }
 }
