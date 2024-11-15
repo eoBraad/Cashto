480670de890f132e70844b8477f65a02a023b56e
EoBrad <joao.alencar099@gmail.com>
2024-11-15 12:22:25 -0300

    Feat: Add base Entity class

---
+++ src/Cashto.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Cashto.Domain.Entities;
+
+public class BaseEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
