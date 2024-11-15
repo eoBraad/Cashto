53e14ae0cfc3d571278c95d3775a61657d591378
EoBrad <joao.alencar099@gmail.com>
2024-11-15 17:05:28 -0300

    wip

--- src/Cashto.Infrastructure/DependencyInjectionExtension.cs
+++ src/Cashto.Infrastructure/DependencyInjectionExtension.cs
@@ -1,4 +1,8 @@
-﻿using Cashto.Infrastructure.Database;
+﻿using Cashto.Domain.Repositories;
+using Cashto.Domain.Repositories.User;
+using Cashto.Infrastructure.Database;
+using Cashto.Infrastructure.Repositories;
+using Cashto.Infrastructure.Repositories.User;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,5 +19,14 @@ public static class DependencyInjectionExtension
         {
             opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), x => x.MigrationsAssembly("Cashto.Infrastructure"));
         });
+
+        AddRepositories(services);
+    }
+
+    public static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IUserReadOnlyRepository, UserReadOnlyRepository>()
+            .AddScoped<IUserWriteOnlyRepository, UserWriteOnlyRepository>()
+            .AddScoped<IWorkUnity, WorkUnity>();
     }
 }
