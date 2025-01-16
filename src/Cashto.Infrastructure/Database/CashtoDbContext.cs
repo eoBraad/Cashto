64d1eb15d5f77938f73c80dce57abfa0bc1d3330
EoBrad <joao.alencar099@gmail.com>
2025-01-16 11:47:04 -0300

    Feat: Add Entities in the DbContext

--- src/Cashto.Infrastructure/Database/CashtoDbContext.cs
+++ src/Cashto.Infrastructure/Database/CashtoDbContext.cs
@@ -6,4 +6,8 @@ namespace Cashto.Infrastructure.Database;
 public class CashtoDbContext(DbContextOptions<CashtoDbContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+
+    public DbSet<Account> Accounts { get; set; }
+
+    public DbSet<Transaction> Transactions { get; set; }
 }
