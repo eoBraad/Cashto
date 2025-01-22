97e6e18ec639c92132789efd3b6a36c889d972c7
EoBrad <joao.alencar099@gmail.com>
2025-01-22 15:42:57 -0300

    Feat: - Implements the Repository interface to Entity Account

---
+++ src/Cashto.Domain/Repositories/Account/IAccountReadOnlyRepository.cs
@@ -0,0 +1,6 @@
+namespace Cashto.Domain.Repositories.Account;
+
+public interface IAccountReadOnlyRepository : IBaseRepositoryReadOnly<Entities.Account>
+{
+    Task<Entities.Account> GetAccountByUserIdAsync(Guid userId);
+}
---
+++ src/Cashto.Infrastructure/Repositories/Account/AccountRepository.cs
@@ -0,0 +1,30 @@
+using Cashto.Domain.Repositories;
+using Cashto.Domain.Repositories.Account;
+using Cashto.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cashto.Infrastructure.Repositories.Account;
+
+public class AccountRepository(CashtoDbContext context, IWorkUnity workUnity) : IAccountReadOnlyRepository, IAccountWriteOnlyRepository
+{
+    public async Task<Domain.Entities.Account> GetByIdAsync(Guid id)
+    {
+        return await context.Accounts.AsNoTracking().FirstOrDefaultAsync(a => a.Id == id);
+    }
+
+    public Task<List<Domain.Entities.Transaction>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Domain.Entities.Account> GetAccountByUserIdAsync(Guid userId)
+    {
+        return await context.Accounts.AsNoTracking().FirstOrDefaultAsync(a => a.UserId == userId);
+    }
+
+    public async Task AddAsync(Domain.Entities.Account entity)
+    {
+        await context.Accounts.AddAsync(entity);
+        await workUnity.Commit();
+    }
+}
