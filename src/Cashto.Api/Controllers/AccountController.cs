52a78d758214b4d991118b19120194804b78865e
João Pontes <joao.alencar099@gmail.com>
2025-01-23 11:44:51 -0300

    feat(account): implement new account creation feature
    Added functionality to allow users to create new accounts. This includes input validation and error handling for duplicate entries.

---
+++ src/Cashto.Api/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+using Cashto.Application.UseCases.Account.Create;
+using Cashto.Communication.Requests.Account;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cashto.Api.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController : ControllerBase
+{
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> Create([FromBody] CreateAccountRequestJson request, [FromServices] ICreateAccountUseCase createAccountUseCase)
+    {
+        var userId = base.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Sid)?.Value;
+
+        await createAccountUseCase.Execute(request, userId!);
+
+        return Created("", null);
+    }
+}
