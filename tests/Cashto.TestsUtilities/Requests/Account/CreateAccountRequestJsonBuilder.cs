a743b6b0ca03103c4be48a954add7ef25bc5b93f
João Pontes <joao.alencar099@gmail.com>
2025-01-28 13:10:55 -0300

    test (CreateAccountValidator): create validator test to createAccount.

---
+++ tests/Cashto.TestsUtilities/Requests/Account/CreateAccountRequestJsonBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using Cashto.Communication.Requests.Account;
+
+namespace Cashto.TestsUtilities.Requests.Account;
+
+public class CreateAccountRequestJsonBuilder
+{
+    public static CreateAccountRequestJson Build()
+    {
+        return new Faker<CreateAccountRequestJson>()
+            .RuleFor(a => a.Balance, f => f.Random.Decimal())
+            .RuleFor(a => a.Name, f => f.Company.CompanyName());
+    }
+}
---
+++ tests/Cashto.ValidatorsTest/Account/CreateAccountValidatorTest.cs
@@ -0,0 +1,55 @@
+﻿using Cashto.Application.UseCases.Account.Create;
+using Cashto.Exception;
+using Cashto.TestsUtilities.Requests.Account;
+using FluentAssertions;
+
+namespace Cashto.ValidatorsTest.Account;
+
+public class CreateAccountValidatorTest
+{
+    [Fact]
+    public void Validate_Valid_Create_Account_Request()
+    {
+        // Arrange
+        var validator = new CreateAccountValidator();
+        var createAccountRequest = CreateAccountRequestJsonBuilder.Build();
+
+        // Act
+        var result = validator.Validate(createAccountRequest);
+
+        // Assert
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void Name_Cannot_Be_Empty(string name)
+    {
+        var validator = new CreateAccountValidator();
+        var createAccountRequest = CreateAccountRequestJsonBuilder.Build();
+        createAccountRequest.Name = name;
+
+        var result = validator.Validate(createAccountRequest);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.ACCOUNT_NAME_EMPTY));
+    }
+
+    [Theory]
+    [InlineData("a")]
+    [InlineData("ab")]
+    public void Name_Must_Be_At_Least_4_Characters(string name)
+    {
+        var validator = new CreateAccountValidator();
+        var createAccountRequest = CreateAccountRequestJsonBuilder.Build();
+        createAccountRequest.Name = name;
+
+        var result = validator.Validate(createAccountRequest);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.ACCOUNT_NAME_MINIMUM));
+    }
+}
