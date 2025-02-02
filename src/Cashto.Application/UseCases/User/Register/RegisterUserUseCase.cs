37d1f4a013b4a88d23eff7d0a1569594166b08bc
João Pontes <joao.alencar099@gmail.com>
2025-02-02 13:53:50 -0300

    refact: Fixed null references and converted to a primary constructor

--- src/Cashto.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Cashto.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -9,42 +9,28 @@ using FluentValidation.Results;
 
 namespace Cashto.Application.UseCases.User.Register;
 
-public class RegisterUserUseCase : IRegisterUserUseCase
+public class RegisterUserUseCase(
+    IUserReadOnlyRepository userReadOnlyRepository,
+    IUserWriteOnlyRepository userWriteOnlyRepository,
+    IMapper mapper,
+    IPasswordEncripter passwordEncripter,
+    IWorkUnity workUnity)
+    : IRegisterUserUseCase
 {
-    
-    private readonly IUserReadOnlyRepository _userReadOnlyRepository;
-    private readonly IUserWriteOnlyRepository _userWriteOnlyRepository;
-    private readonly IMapper _mapper;
-    private readonly IPasswordEncripter _passwordEncripter;
-    private readonly IWorkUnity _workUnity;
-
-    public RegisterUserUseCase(
-        IUserReadOnlyRepository userReadOnlyRepository,
-        IUserWriteOnlyRepository userWriteOnlyRepository,
-        IMapper mapper,
-        IPasswordEncripter passwordEncripter,
-        IWorkUnity workUnity)
-    {
-        _userReadOnlyRepository = userReadOnlyRepository;
-        _userWriteOnlyRepository = userWriteOnlyRepository;
-        _mapper = mapper;
-        _passwordEncripter = passwordEncripter;
-        _workUnity = workUnity;
-    }
     public async Task Execute(RegisterUserRequestJson request)
     {
         await Validate(request);
-        var user = _mapper.Map<Domain.Entities.User>(request);
-        user.Password = _passwordEncripter.Encrypt(request.Password);
-        await _userWriteOnlyRepository.AddAsync(user);
-        await _workUnity.Commit();
+        var user = mapper.Map<Domain.Entities.User>(request);
+        user.Password = passwordEncripter.Encrypt(request.Password!);
+        await userWriteOnlyRepository.AddAsync(user);
+        await workUnity.Commit();
     }
 
     private async Task Validate(RegisterUserRequestJson request)
     {
         var result = new RegisterUserValidator().Validate(request);
 
-        var emailExists = await _userReadOnlyRepository.VerifyUserEmailExists(request.Email);
+        var emailExists = await userReadOnlyRepository.VerifyUserEmailExists(request.Email!);
 
         if (emailExists > 0)
         {
