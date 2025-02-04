e57d24edad2712ce6407114edc5fdc7f4736c112
EoBrad <joao.alencar099@gmail.com>
2025-02-04 11:06:29 -0300

    refact (Base Repository): Refactor base repository write only to include 2 new methods UpdateAsync and Delete Async

--- src/Cashto.Domain/Repositories/IBaseRepositoryWriteOnly.cs
+++ src/Cashto.Domain/Repositories/IBaseRepositoryWriteOnly.cs
@@ -3,4 +3,8 @@ namespace Cashto.Domain.Repositories;
 public interface IBaseRepositoryWriteOnly<T>
 {
     Task AddAsync(T entity);
+
+    Task<T> UpdateAsync(T entity);
+
+    Task DeleteAsync(T entity);
 }
